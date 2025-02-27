509d8385644ef5803c3779b23b1bcc0b627e88a1
zalozhnichek <mednik2005@gmail.com>
2025-02-27 21:34:15 +0300

    rewrited Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class MyClass
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
